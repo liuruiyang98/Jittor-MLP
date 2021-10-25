d2a850695816ad84247519fb91c163ff920def70
liuruiyang98 <865296294@qq.com>
2021-10-25 14:34:51 +0800

    Remove ConvMixerV2 in pytorch init.py

--- models_pytorch/__init__.py
+++ models_pytorch/__init__.py
@@ -3,7 +3,7 @@ from .res_mlp import ResMLPForImageClassification
 from .mlp_mixer import MLPMixerForImageClassification
 from .vip import ViP
 from .s2_mlp_v2 import S2MLPv2
-from .conv_mixer import ConvMixer, ConvMixerV2
+from .conv_mixer import ConvMixer
 from .conv_mlp import convmlp_s, convmlp_l, convmlp_m
 from .raft_mlp import RaftMLP
 from .sparse_mlp import SparseMLP
